7431da6b3399d9b260c52404d7ffdc858cde9283
James Miller <james.miller@student.unsw.edu.au>
2021-03-01 18:51:31 +1100

    data fetching done

--- API/scraper/helpers.py
+++ API/scraper/helpers.py
@@ -1,14 +1,23 @@
 import re
 
 def findDate(string):
-    return re.search('$.+ \<|$', string).group()
+    return re.search('^.+(?= <a h)|$', string).group()
+
+def findID(string):
+    return re.search('(?<=id=\"id)\d+(?=\")|$', string).group()
+
+def findName(string):
+    return re.search('<a.*> (.+?)</a>', string).group(1)
+
+def printInfo(key, date, sid, name):
+    print(f"{key}: {date} : {sid} : {name}")
 
 def processData(jsonResponse):
     contents = jsonResponse['contents']
     for key in contents:
         for item in contents[key]:
-            print(str(key), end=": ")
-            """ date = findDate(item)
-            print(date, end="") """
-            print(item)
+            date  = findDate(item)
+            sid   = findID(item)
+            name  = findName(item)
+            printInfo(key, date, sid, name)
     pass
