b1564ab325fb7c63ce0b4006add79304f9338d3a
James Miller <james.miller@student.unsw.edu.au>
2021-03-09 16:28:34 +1100

    Schema further update

--- API/database_Schema.sql
+++ API/database_Schema.sql
@@ -2,30 +2,51 @@
 
 /*  CREATING TABLES */
 
-CREATE TABLE [IF NOT EXISTS] locations (
+CREATE TABLE IF NOT EXISTS locations (
    locationID INTEGER CHECK (markerID > 0) PRIMARY KEY,                 /*  */
    latitude NUMERIC(10, 6) NOT NULL,                                    /*  */
    longitude NUMERIC(10, 6) NOT NULL,                                   /*  */
    locationName VARCHAR(100) NOT NULL                                   /*  */
 );
 
-CREATE TABLE [IF NOT EXISTS] articles (
+CREATE TABLE IF NOT EXISTS articles (
     articleID INTEGER NOT NULL CHECK (articleID > 0) PRIMARY KEY,       /* Primary Key */
-    pub_Date DATE NOT NULL,                                             /*  */
+    pubDate DATE NOT NULL,												/*  */
     articleName VARCHAR(100),                                           /* HEADLINE */
     locationID INTEGER CHECK (markerID > 0),                            /*  */
     mainText TEXT NOT NULL,                                             /*  */
     linkToArticle TEXT NOT NULL,                                        /* URL */
-    FOREIGN KEY (markerID) REFERENCES markerLocations(markerID)         
+    FOREIGN KEY (markerID) REFERENCES markerLocations(markerID)
 );
 
-CREATE TABLE [IF NOT EXISTS] reports (
+CREATE TABLE IF NOT EXISTS reports (
     reportID SERIAL PRIMARY KEY,                                        /* Primary Key */
     articleID INTEGER not null,                                         /*  */
+    locationID INTEGER,                                                 /* IF NULL get location from article location*/
     disease varchar(100) not null,                                      /*  */
     syndrome varchar(100) not null,                                     /*  */
     eventDate DATE,                                                     /* IF NULL get date from article */
-    locationID INTEGER,                                                 /* IF NULL get location from article location*/
-    FOREIGN KEY (articleID) REFERENCES articles(articleID)              /*  */
-    FOREIGN KEY (reportLocation) REFERENCES locations(locationID)       /*  */
+    FOREIGN KEY (articleID) REFERENCES articles(articleID)
+    FOREIGN KEY (reportLocation) REFERENCES locations(locationID)
 );
+
+CREATE TABLE IF NOT EXISTS metaData (
+	lastUpdated TIMESTAMP not null,
+
+);
+
+CREATE FUNCTION check_reports_insert() 
+	RETURNS TRIGGER 
+	LANGUAGE PLPGSQL
+	AS $$
+	BEGIN
+		NEW.LOCATIONID =  COALESCE ( ( OLD.LOCATIONID ), ( SELECT  LOCATIONID  from  articles  where  articles.articleID == old.articleID ) );
+		NEW.EVENTDATE  =  COALESCE ( ( OLD.EVENTDATE  ), ( SELECT  PUBDATE     from  articles  where  articles.articleID == old.articleID ) );
+		RETURN NEW;
+	END;
+	$$
+
+CREATE TRIGGER IF NOT EXISTS reports_Insert
+	BEFORE INSERT ON REPORTS
+	FOR EACH ROW WHEN reports.locationID IS NULL OR reports.eventDate IS NULL
+	EXECUTE PROCEDURE check_reports_insert();
