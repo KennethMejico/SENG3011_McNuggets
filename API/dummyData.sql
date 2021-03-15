598641119e321137f9eff8a61aa91a8929af44fe
James Miller <james.miller@student.unsw.edu.au>
2021-03-16 00:05:38 +1100

    I thought I already commited this :/

--- API/dummyData.sql
+++ API/dummyData.sql
@@ -1,7 +1,11 @@
 
+/* Locations (LocationID,Latitude,Longitude,LocationName )*/
+/* Articles (ArticleID, PubDate , ArticleName , LocationID , MainText , LinkToArticle )*/
+/* Reports ( ReportID (SERIAL), ArticleID, LocationID, Disease, Syndrome, EventDate)  ;*/
+
 INSERT INTO LOCATIONS (106, 39.966930, -97.556099, "United States");
 
-INSERT INTO Articles (7774890, 13 Sep 2020,  West Nile virus (14): Americas, USA (MA), 106, 
+INSERT INTO Articles (7774890, 13 Sep 2020,  'West Nile virus (14): Americas, USA (MA)', 106, 
 "WEST NILE VIRUS (14): AMERICAS, USA (MASSACHUSETTS )
 ****************************************************
 A ProMED-mail post
@@ -51,3 +55,62 @@ West Nile virus: Americas (USA) mosquitoes 20200317.7103719
 .................................................sb/jh/ty/msp/jh", https://promedmail.org/promed-post/?id=7774890 );
 
 INSERT INTO Reports (7774890, 106, 'WEST NILE VIRUS', 'a fever, experience headaches, body aches, vomiting, diarrhea, a rash, other side effects', NULL);
+
+INSERT INTO Locations (2975, -27.000000, -50.000000, "Santa Catarina, Brazil");
+
+INSERT INTO Articles (8235917, 09 Mar 2021, 'Yellow fever - Americas (04): Brazil (SC) monkey, susp', 2975, 
+"YELLOW FEVER - AMERICAS (04): BRAZIL (SANTA CATARINA) MONKEY, SUSPECTED
+***********************************************************************
+A ProMED-mail post
+http://www.promedmail.org
+ProMED-mail is a program of the
+International Society for Infectious Diseases
+http://www.isid.org
+
+Date: Fri 5 Mar 2021
+Source: Sul Agora [in Portuguese, trans. Mod.TY, edited]
+https://sulagora.com.br/saude/sobe-para-40-o-numero-de-macacos-encontrados-mortos-na-regiao-5508
+
+
+In a little more than 15 days, the region has registered 40 dead monkeys in the cities of Santa Rosa de Lima, Rio Fortuna, Sao Martinho, Grao-Para and Braco do Norte, with 24 in just one week.
+
+Teams from regional Health Surveillance and the Zoonoses Surveillance Unit (UVZ), Tubarao Municipal Health Foundation (FMS), issued an alert about the possibility of yellow fever cases that could be causing the deaths of monkeys in municipalities of the region.
+
+Several days ago, 8 dead monkeys were collected that were encountered in the Santa Rosa de Lima region, the results of which still have not been issued. However, the alert was made due to the increase in cases.
+
+According to the biologist Sabrina Fernandes Cardoso of the State Health Secretariat, the 1st results of the test are expected at the beginning of next week. \"It is worrisome because there is no apparent cause of death of these animals, with no signs of aggression or accident. In addition to this, the monkeys that were seen constantly by the residents have disappeared,\" she stated.
+
+The alert has been issued because the appearance of dead monkeys in a given region could indicate the presence of the virus that causes yellow fever. When such an event is detected, the viscera of the animals is taken out and sent for testing in the laboratory in order to confirm, or not, the presence of this virus in these animals, which are considered to be sentinels.
+
+\"The entire locality is being mapped for vaccination coverage of the residents. Epidemiological surveillance teams are visiting house-by-house and orienting the residents. A point to underscore is the increase in vaccination against yellow fever, which is available in all the health posts,\" stated Sabrina, the biologist.
+
+Municipal residents who by chance encounter a dead monkey are being instructed to go to the nearest health unit to report it. Then, the health team must note the data and communicate it to the Municipal Epidemiological Surveillance, which will take the necessary measures.
+
+--
+communicated by:
+ProMED-PORT
+<promed-port@promedmail.org>
+
+[The possible (probable) confirmation of yellow fever among the non-human primates under investigation should not be a surprise. On the contrary, it will only confirm the intense circulation of the virus in Santa Catarina, as reported by ProMED-PORT. - Mod.RNA
+
+The deaths of monkeys in a given area over a short period is characteristic of the sylvan (forest) yellow fever virus transmission cycle. The prompt response of local health authorities to alert the local population to the situation and determine yellow fever vaccination coverage is prudent.
+
+Active transmission of the virus among monkeys carries with it the risk of spillover to humans and ongoing transmission in the urban cycle, which could spread rapidly in a human population with inadequate vaccination coverage. - Mod.TY
+
+HealthMap/ProMED map:
+Santa Catarina, Brazil: http://healthmap.org/promed/p/2975]
+See Also
+Yellow fever - Americas (03): Trinidad & Tobago, monkey die offs 20210226.8216422
+Yellow fever - Americas (02): Brazil (PR) monkeys 20210208.8174576
+Yellow fever - Americas (01): Brazil (SC) monkey 20210207.8172394
+2020
+---
+Yellow fever - Americas (14): Brazil (PR) monkeys 20201227.8050746
+Yellow fever - Americas (13): Brazil (PR) monkey 20201211.8010059
+Yellow fever - Americas (12): Brazil (PR) monkeys 20201129.7978944
+Yellow fever - Americas (06): Brazil, monkey, conservation concern 20200403.7180314
+Yellow fever - Americas (03): Brazil, monkey, alert 20200203.6948827
+Yellow fever - Americas (02): Brazil, monkey, alert 20200120.6900120
+.................................................rna/sh/ty/msp/sh", https://promedmail.org/promed-post/?id=8235917);
+
+INSERT INTO REPORTS (8235917, 2975, "Yellow Fever", "", NULL);
