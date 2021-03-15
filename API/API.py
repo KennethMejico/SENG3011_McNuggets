f95eeef57496727f5bbca476316f610ed147a91a
Kenneth <z5257133@ad.unsw.edu.au>
2021-03-15 21:43:50 +1100

    Completed semi-working conversion for articles to JSON

--- API/API.py
+++ API/API.py
@@ -6,6 +6,15 @@ from datetime import datetime
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+app.config['JSON_SORT_KEYS'] = False
+
+def convert(lst):
+    reports_list = []
+    # This needs to be updated in future when multiple reports are assigned to a single article
+    report_dict = {'event_date':lst[4], 'locations':lst[5], 'diseases': lst[6], 'syndromes': lst[7]}
+    reports_list.append(report_dict)
+    article_dict = {'url':lst[0], 'date_of_publication':lst[1], 'headline':lst[2], 'main_text':lst[3], 'reports': reports_list}
+    return article_dict
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -23,24 +32,10 @@ def filter_search():
     key_terms = query_parameters.get('key_terms')
     location = query_parameters.get('location')
 
-    query = ""
-    to_filter = []
-
-    if start_date:
-        query += ' start_date=? AND'
-        to_filter.append(start_date)
-    if end_date:
-        query += ' end_date=? AND'
-        to_filter.append(end_date)
-    if key_terms:
-        query += ' key_terms=? AND'
-        to_filter.append(key_terms)
-    if location:
-        query += ' location=? AND'
-        to_filter.append(location)
-    if not (start_date or end_date or location):
+    if not (start_date or end_date):
         return page_not_found(404)
 
+    # Connect to MySQL Database
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -49,21 +44,51 @@ def filter_search():
         database="promedmail"
     )
 
+    # Convert start_date and end_date to datetime
     start = datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S')
     end = datetime.strptime(end_date, '%Y-%m-%dT%H:%M:%S')
+
+    # Execute query to gather articles which satisfy data constraint
     mycursor = mydb.cursor()
-    sql = "select * from articles where pubDate >= %s and pubDate <= %s;"
-    sql2 = "select * from locations;"
-    data = (start, end)
-    mycursor.execute(sql, data)
-    record = mycursor.fetchall()
 
+    # Case 1: Only period of interest input
+    if (start_date or end_date and not key_terms and not location):
+        start = datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S')
+        end = datetime.strptime(end_date, '%Y-%m-%dT%H:%M:%S')
+        sql = """
+            select
+                a.linkToArticle,
+                a.pubDate,
+                a.articleName,
+                a.mainText,
+                r.eventDate,
+                l.locationName,
+                r.disease,
+                r.syndrome
+            from
+                articles a
+            left join
+                reports r on r.articleID = a.articleID
+            left join
+                locations l on r.locationID = l.locationID
+            where
+                pubDate >= %s and pubDate <= %s;
+        """
+        data = (start, end)
+        mycursor.execute(sql, data)
+        results = mycursor.fetchall()
+        print(results)
+        article_list = []
+        for article in results:
+            article_list.append(convert(article))
+        
+        print(article_list)
+    # Case 2: Only key terms --
+    # Case 3: Only locations --
+    # Case 4: Period of interest and key terms
+    # Case 5: Period of interest and locations
+    # Case 6: Key terms and locations
+    # Case 7: All Three
 
-    # Connect to DB
-    # Query Articles DB for publication date
-    # If articles reports do not include key_terms or location, remove from dict
-
-    results = record
-    return jsonify(results)
-
+    return jsonify(article_list)
 app.run()
