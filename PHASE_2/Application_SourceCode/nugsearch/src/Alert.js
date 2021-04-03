5778843d74337d3ef4521273cc259e091cc4eff5
Luke Chen <lchen8801@gmail.com>
2021-04-03 23:14:05 +1000

    Started Alert page

---
+++ PHASE_2/Application_SourceCode/nugsearch/src/Alert.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Alert.css'
+
+class Alert extends React.Component {
+    render() {
+        return(
+            <div className="Background">
+                <h1 className="Heading"> Alert: COVID19 Outbreak </h1>
+                <div></div>
+            </div>
+        )
+    }
+}
+
+export default Alert;
