c9d1bc5f41da29f346937f98723cc8281af28e24
Liam <61527066+bubble-hiss@users.noreply.github.com>
2021-04-03 15:36:15 +1100

    Changed background, added navbar

--- PHASE_2/Application_SourceCode/nugsearch/src/App.js
+++ PHASE_2/Application_SourceCode/nugsearch/src/App.js
@@ -1,9 +1,20 @@
 import logo from './logo.svg';
+import nugLogo from './nugSearchLogo300.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <ul className="NavBar">
+          <li className="NavBarLi"><img src={nugLogo} alt="logo" className="SmallLogo"/></li>
+          <li className="NavBarLi LogoText">NugSearch</li>
+          <li className="NavBarLi"><a href="default.asp">Home</a></li>
+          <li className="NavBarLi"><a href="news.asp">About</a></li>
+          <li className="NavBarLi"><a href="contact.asp">Contact</a></li>
+          <li className="NavBarLi"><a href="about.asp">Alerts</a></li>
+        </ul>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
