e5c87f63922515505f335962fa47ca804128c70b
Liam <61527066+bubble-hiss@users.noreply.github.com>
2021-04-05 17:15:50 +1000

    Alert page is dynamically loaded

--- PHASE_2/Application_SourceCode/nugsearch/api/api.py
+++ PHASE_2/Application_SourceCode/nugsearch/api/api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -15,10 +15,25 @@ def search():
         "result": "Test result"
     }
 
-@app.route('/getAlert')
-def getAlert():
-    return {}
+@app.route('/getAlerts')
+def getAlerts():
+    return {
+        "alerts": ["Covid-19", "Black Death"]
+    }
 
 @app.route('/getAlertDescription')
 def getAlertDescription():
-    return {}
+    if "Covid-19" == request.args.get("name"):
+        return {
+            "title": "Covid-19 Outbreak",
+            "text": "There were 17 cases of COVID-19 in Sydney on 10/04/2021. In the past, this amount of cases has lead to a lockdown in your area. Be prepared for another to happen again. COVID-19 has caused a lockdown in your area THREE times before. COVID-19 has caused lockdowns to happen way too many times in total. The last COVID-19 lockdown happened after 15 cases were reported in a day. See government alerts for your area: https://www.nsw.gov.au/"
+        }
+    elif "Black Death" == request.args.get("name"):
+        return {
+            "title": "Black Death Resurgeance",
+            "text": "Somehow, the black death has returned"
+        }
+    return {
+        "title": "Bad Alert string",
+        "text": "Bad Alert string"
+    }
--- PHASE_2/Application_SourceCode/nugsearch/src/Alert.js
+++ PHASE_2/Application_SourceCode/nugsearch/src/Alert.js
@@ -1,16 +1,56 @@
 import React from 'react'
 import './Alert.css'
 
-import covidImg from './COVID-19-1080x515.jpg'
-import alertImg from './Health-alert.png'
+//import alertImg from './Health-alert.png'
+
+//                <img src={covidImg} alt="img" class="CovidImage"/>
 
 class Alert extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alertTitle: "",
+            alertText: ""
+        }
+
+        fetch('/getAlerts').then(res => res.json()).then(data => {
+            if (data.alerts.length === 0) {
+                this.setState({alertTitle: "None"});
+                /*this.state = {
+                    alertTitle: "None",
+                    alertText: "No lockdowns seem imminent"
+                }*/
+            }
+            else {
+                fetch("/getAlertDescription?name=" + data.alerts[0])
+                .then(res => res.json())
+                .then(data => {
+                    this.setState({
+                        alertTitle: data.title,
+                        alertText: data.text
+                    })
+                    /*this.state = {
+                        alertTitle: data.title,
+                        alertText: data.text
+                    }*/
+                });
+            }
+        });
+    }
+
     render() {
         return(
+        <div>
             <div className="Background">
-                <h1 className="Heading"> Alert: COVID19 Outbreak </h1>
-                <img src={covidImg} alt="img" class="CovidImage"/>
-                <p class="AlertText"> There were 17 cases of COVID-19 in Sydney on 10/04/2021.
+                <h1 className="Heading">Alert: {this.state.alertTitle}</h1>
+                <p className="AlertText">{this.state.alertText}</p>
+            </div>
+        </div>
+        )
+    }
+}
+/*
+There were 17 cases of COVID-19 in Sydney on 10/04/2021.
 
 In the past, this amount of cases has lead to a lockdown in your area. Be prepared for another to happen again.
 
@@ -19,23 +59,23 @@ COVID-19 has caused a lockdown in your area THREE times before. COVID-19 has cau
 The last COVID-19 lockdown happened after 15 cases were reported in a day.
 
 See government alerts for your area: https://www.nsw.gov.au/
-                </p>
-                <div class="Divider"></div>
+*/
+/*
+            <div className="OtherAlerts">
+                <div className="Divider"></div>
                 <h2 className="OtherHeading"> Other Alerts </h2>
-                <div class="row">
-                    <div class="column">
-                        <img src={alertImg} alt="alert1" class="Alert1" style={{ width: '80%' }}/>
+                <div className="row">
+                    <div className="column">
+                        <img src={alertImg} alt="alert1" className="Alert1" style={{ width: '80%' }}/>
                     </div>
-                    <div class="column">
-                        <img src={alertImg} alt="alert2" class="Alert2" style={{ width: '80%' }}/>
+                    <div className="column">
+                        <img src={alertImg} alt="alert2" className="Alert2" style={{ width: '80%' }}/>
                     </div>
-                    <div class="column">
-                        <img src={alertImg} alt="alert3" class="Alert3" style={{ width: '80%' }}/>
+                    <div className="column">
+                        <img src={alertImg} alt="alert3" className="Alert3" style={{ width: '80%' }}/>
                     </div>
                 </div>
             </div>
-        )
-    }
-}
+*/
 
 export default Alert;
