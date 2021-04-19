998e11a6cafaef3d57e84390d34da9118055bc39
Liam <61527066+bubble-hiss@users.noreply.github.com>
2021-04-19 20:33:32 +1000

    Reorganised repository

--- PHASE_2/Application_SourceCode/nugsearch/src/signup/Signup.js
+++ PHASE_2/Application_SourceCode/nugsearch/src/signup/Signup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Signup.css'
 import { withRouter } from 'react-router-dom';
-import nugLogo from './nugSearchLogo300.png';
+import nugLogo from '../images/nugSearchLogo300.png';
 
 class Signup extends React.Component {
     constructor(props) {
